04638bd93ae287646089ca5bb9e61d2af6a0029e
Karanveer Singh <karanveersingh7@rrc.ca>
2025-09-07 13:33:49 -0500

    Changed populate employees function

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 // array of terms to be displayed on the page
 const terms = ["SEO", "Abstraction", "IDE", "OOP", "Runtime"];
 
-const populateTermList = (listNode, terms) => {
-    // iterate over term array
-    terms.forEach(term => {
-        // create new DOM node
-        const newLiNode = document.createElement("li");
+const populateEmployees = (listNode, employees) => {
+  employees.forEach((dept) => {
+    // Create department title
+    const deptHeading = document.createElement("h4");
+    deptHeading.textContent = dept.department;
 
-        // modify HTML in new DOM node to include iterated term value
-        newLiNode.innerHTML = `<a href="#">${term}</a>`;
-
-        // append new LI node as child element of list argument node
-        listNode.appendChild(newLiNode);
+    // Create employee list
+    const ul = document.createElement("ul");
+    dept.names.forEach((name) => {
+      const li = document.createElement("li");
+      li.textContent = name;
+      ul.appendChild(li);
     });
-}
 
+    // Wrap in section
+    const section = document.createElement("section");
+    section.classList.add("department");
+    section.appendChild(deptHeading);
+    section.appendChild(ul);
+
+    // Append to container
+    listNode.appendChild(section);
+  });
+};
 
 /** 
 // add event listener to the "document" node
 // callback function invokes when event "fires" on element that listener was
 appended to, in this case, when the DOM has loaded fully
-*/ 
+*/
 document.addEventListener("DOMContentLoaded", () => {
-    // store the DOM node referencing an element with class "top-terms__list"
-    const termListNode = document.querySelector(".top-terms__list");
+  // store the DOM node referencing an element with class "top-terms__list"
+  const termListNode = document.querySelector(".top-terms__list");
 
-    // invoke function passing in DOM node and terms array
-    populateTermList(termListNode, terms);
+  // invoke function passing in DOM node and terms array
+  populateTermList(termListNode, terms);
 });
