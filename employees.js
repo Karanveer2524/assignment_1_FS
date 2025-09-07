e8df5e60563cb1f124951f69f7f2e759c09be256
Karanveer Singh <karanveersingh7@rrc.ca>
2025-09-07 13:25:02 -0500

    Added employees.js

---
+++ employees.js
@@ -0,0 +1,14 @@
+const employees = [
+  {
+    department: "Administration",
+    names: ["Zoë Robins", "Madeleine Madden"],
+  },
+  {
+    department: "Audit",
+    names: ["Josha Sadowski", "Kate Fleetwood"],
+  },
+  {
+    department: "Banking Operations",
+    names: ["Priyanka Bose", "Hammed Animashaun"],
+  },
+];
